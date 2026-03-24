6b36f4b4b2d2b498e466d478c970cdc744d480e6
BenBrooke450 <bensir@mail.com>
2026-03-24 17:31:26 +0100

    UPDATE

--- TinyLlama/TinyLlama.py
+++ TinyLlama/TinyLlama.py
@@ -1,4 +1,6 @@
 
+import torch
+
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
@@ -6,3 +8,15 @@ model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id)
+
+
+prompt = "Explain drug discovery in simple terms"
+
+inputs = tokenizer(prompt, return_tensors="pt")
+
+outputs = model.generate(
+    **inputs,
+    max_new_tokens=100
+)
+
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
