4ebcdd91d7e63743ac88972a76fba533deb6c883
BenBrooke450 <bensir@mail.com>
2026-03-24 17:52:29 +0100

    UPDATE

--- TinyLlama/TinyLlama.py
+++ TinyLlama/TinyLlama.py
@@ -1,21 +1,27 @@
 
 
 
-class LLM_Tiny_Llama():
+class LLM_Tiny_Llama:
     def __init__(self):
         import torch
         from transformers import AutoTokenizer, AutoModelForCausalLM
 
-        tokenizer = AutoTokenizer.from_pretrained("./tinyllama")
-        model = AutoModelForCausalLM.from_pretrained("./tinyllama")
+        MODEL_PATH = "/Users/benjaminbrooke/.cache/huggingface/hub/models--TinyLlama--TinyLlama-1.1B-Chat-v1.0/snapshots/fe8a4ea1ffedaf415f4da2f062534de366a451e6"
+
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+        self.model = AutoModelForCausalLM.from_pretrained( MODEL_PATH)
 
     def prompt(self, prompt):
 
-        inputs = tokenizer(prompt, return_tensors="pt")
+        inputs = self.tokenizer(prompt, return_tensors="pt")
 
-        outputs = model.generate(
-            **inputs,
-            max_new_tokens=100
-        )
+        outputs = self.model.generate(**inputs,max_new_tokens=100)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+
+if __name__ == "__main__":
+
+    first_model = LLM_Tiny_Llama()
+
+    print(first_model.prompt("Hello, is this the Tiny LLama model?"))
